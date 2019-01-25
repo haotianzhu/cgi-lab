a41e501ae153fc195de9379cbcbcbfa392bd4570
haotianzhu <zhuhaotian19970215@icloud.com>
2019-01-24 18:12:58 -0700

    done

--- secret.py
+++ secret.py
@@ -12,8 +12,8 @@ class FollowingTheTAsInstructionsError(Exception):
         ))
 
 # Delete this line:
-raise FollowingTheTAsInstructionsError
+# raise FollowingTheTAsInstructionsError
 
 # Edit the following two lines:
-username = "<pick a username here>"
-password = "<pick a password here>"
+username = "haotian"
+password = "s3cr3t!Help"
